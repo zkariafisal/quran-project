f82e41a63feb2c870ba3bb7835cf8be027276460
zkariafisal <149721727+zkariafisal@users.noreply.github.com>
2026-05-27 01:01:38 +0300

    Add files via upload

---
+++ src/components/Home/SurahsList/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import * as customPropTypes from 'customPropTypes';
+import debug from 'helpers/debug';
+import Link from 'react-router/lib/Link';
+
+const styles = require('./style.scss');
+
+const SurahsList = (props) => {
+  debug('component:Index', 'SurahsList');
+
+  return (
+    <ul className="col-md-4 list-unstyled">
+      {props.chapters.map(chapter => (
+        <li className={`${styles.item}`} key={chapter.id}>
+          <Link to={`/${chapter.id}`} className={`${styles.link} row`}>
+            <div className="col-xs-2 text-muted">
+              {chapter.chapterNumber}
+            </div>
+            <div className="col-xs-7">
+              {chapter.nameSimple}
+            </div>
+            <div className={`col-xs-3 text-left ${styles.arabic}`}>
+              <span className={`icon-surah${chapter.id}`} />
+            </div>
+
+            <div
+              className={`col-xs-10 col-xs-offset-2 ${styles.translated_name}`}
+            >
+              <span
+                className={`text-uppercase ${chapter.translatedName.languageName}`}
+              >
+                {chapter.translatedName.name}
+              </span>
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+SurahsList.propTypes = {
+  chapters: customPropTypes.chapters.isRequired
+};
+
+export default SurahsList;

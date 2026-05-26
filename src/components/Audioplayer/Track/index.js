ed2519dff40fed6facf04b4311c47c61814f4edc
zkariafisal <149721727+zkariafisal@users.noreply.github.com>
2026-05-27 01:09:59 +0300

    Add files via upload

---
+++ src/components/Audioplayer/Track/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { Component, PropTypes } from 'react';
+
+const styles = require('./style.scss');
+
+export default class Track extends Component {
+  static propTypes = {
+    progress: PropTypes.number.isRequired,
+    onTrackChange: PropTypes.func.isRequired
+  };
+
+  handleClick = (event) => {
+    const { onTrackChange } = this.props;
+
+    const fraction = (
+      event.nativeEvent.offsetX /
+      this.container.getBoundingClientRect().width
+    );
+
+    return onTrackChange(fraction);
+  };
+
+  render() {
+    const { progress } = this.props;
+
+    return (
+      <div
+        ref={(container) => { this.container = container; }}
+        className={styles.container}
+        onClick={this.handleClick}
+      >
+        <div className={styles.progress} style={{ width: `${progress}%` }} />
+      </div>
+    );
+  }
+}

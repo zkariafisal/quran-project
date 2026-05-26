881d3e1949c774c18bfab9844a06d40c64648b8a
zkariafisal <149721727+zkariafisal@users.noreply.github.com>
2026-05-27 00:41:24 +0300

    Add files via upload

---
+++ src/components/VersesDropdown/index.js
@@ -0,0 +1,80 @@
+import React, { Component, PropTypes } from 'react';
+import * as customPropTypes from 'customPropTypes';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import { Link } from 'react-scroll';
+import LocaleFormattedMessage from 'components/LocaleFormattedMessage';
+
+const style = require('./style.scss');
+
+class VersesDropdown extends Component {
+  renderItem = (ayah, index) => {
+    const { chapter, loadedVerses, isReadingMode, onClick } = this.props;
+    const number = index + 1;
+
+    if (loadedVerses.has(number) && !isReadingMode) {
+      return (
+        <li key={index}>
+          <Link
+            onClick={() => onClick(number)}
+            to={`verse:${chapter.chapterNumber}:${number}`}
+            smooth
+            spy
+            offset={-120}
+            activeClass="active"
+            duration={500}
+            className="pointer"
+          >
+            {number}
+          </Link>
+        </li>
+      );
+    }
+
+    return (
+      <MenuItem key={index} onClick={() => onClick(number)}>
+        {number}
+      </MenuItem>
+    );
+  }
+
+  renderMenu() {
+    const { chapter } = this.props;
+    const array = Array(chapter.versesCount).join().split(',');
+
+    return array.map((ayah, index) => this.renderItem(ayah, index));
+  }
+
+  render() {
+    const { className } = this.props;
+
+    const title = (
+      <LocaleFormattedMessage id={'setting.verses'} defaultMessage={'Go to verse'} />
+    );
+
+    return (
+      <NavDropdown
+        link
+        className={`dropdown ${className} ${style.dropdown}`}
+        id="verses-dropdown"
+        title={title}
+      >
+        {this.renderMenu()}
+      </NavDropdown>
+    );
+  }
+}
+
+VersesDropdown.propTypes = {
+  loadedVerses: PropTypes.instanceOf(Set).isRequired,
+  chapter: customPropTypes.surahType.isRequired, // Set
+  onClick: PropTypes.func.isRequired,
+  isReadingMode: PropTypes.bool,
+  className: PropTypes.string
+};
+
+VersesDropdown.defaultProps = {
+  className: ''
+};
+
+export default VersesDropdown;

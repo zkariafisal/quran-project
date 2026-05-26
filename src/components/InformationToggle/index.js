0dc4abbe97b71d55db60e3afcd0d8de59e5fc1a2
zkariafisal <149721727+zkariafisal@users.noreply.github.com>
2026-05-27 00:38:13 +0300

    Add files via upload

---
+++ src/components/InformationToggle/index.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react';
+import LocaleFormattedMessage from 'components/LocaleFormattedMessage';
+import { MenuItem } from 'quran-components/lib/Menu';
+
+const InformationToggle = ({ isToggled, onToggle }) => (
+  <MenuItem
+    icon={<i className="ss-icon ss-info vertical-align-middle" />}
+    onClick={() => onToggle({ isShowingSurahInfo: !isToggled })}
+  >
+    <LocaleFormattedMessage id="surah.info" defaultMessage="Surah Info" />
+  </MenuItem>
+);
+
+InformationToggle.propTypes = {
+  isToggled: PropTypes.bool.isRequired,
+  onToggle: PropTypes.func.isRequired
+};
+
+export default InformationToggle;

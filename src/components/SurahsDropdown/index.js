3ec320b494ff066981829a611a0bb2f8df70762b
zkariafisal <149721727+zkariafisal@users.noreply.github.com>
2026-05-27 00:23:26 +0300

    Add files via upload

---
+++ src/components/SurahsDropdown/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import * as customPropTypes from 'customPropTypes';
+import LinkContainer from 'react-router-bootstrap/lib/LinkContainer';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import LocaleFormattedMessage from 'components/LocaleFormattedMessage';
+
+const styles = require('./style.scss');
+
+class SurahsDropdown extends Component {
+  shouldComponentUpdate(nextProps) {
+    return this.props.chapters !== nextProps.chapters;
+  }
+
+  renderList() {
+    const { chapters } = this.props;
+
+    return Object.values(chapters).map((chapter, index) => (
+      <LinkContainer to={`/${chapter.chapterNumber}`} activeClass="active" key={`chapter-${index}`}>
+        <MenuItem>
+          <div className="row">
+            <div className="col-xs-2 col-md-2">
+              <span className="chapter-num">
+                {chapter.chapterNumber}
+              </span>
+            </div>
+            <div className="col-xs-7 col-md-7">
+              <span className="suran-name">{chapter.nameSimple}</span>
+              <br />
+              <span className="chapter-meaning">{chapter.translatedName.name}</span>
+            </div>
+            <div className={`col-xs-3  col-md-3 text-right ${styles.arabicName}`}>
+              {chapter.nameArabic}
+            </div>
+          </div>
+        </MenuItem>
+      </LinkContainer>
+    ));
+  }
+
+  render() {
+    const { chapter } = this.props;
+
+    return (
+      <NavDropdown
+        link
+        className={styles.dropdown}
+        id="chapters-dropdown"
+        title={chapter.nameSimple || <LocaleFormattedMessage id="setting.chapters" defaultMessage="Surahs" />}
+      >
+        {this.renderList()}
+      </NavDropdown>
+    );
+  }
+}
+
+SurahsDropdown.propTypes = {
+  chapters: customPropTypes.chapters.isRequired,
+  chapter: customPropTypes.chapters.isRequired,
+};
+
+export default SurahsDropdown;

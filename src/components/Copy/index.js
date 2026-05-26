83b686d35ee36536fa19b9e22ed49918c62f58cc
zkariafisal <149721727+zkariafisal@users.noreply.github.com>
2026-05-27 00:30:39 +0300

    Add files via upload

---
+++ src/components/Copy/index.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react';
+import copyToClipboard from 'copy-to-clipboard';
+import LocaleFormattedMessage from 'components/LocaleFormattedMessage';
+
+class Copy extends Component {
+
+  state = {
+    isCopied: false
+  };
+
+  handleCopy = () => {
+    copyToClipboard(`${this.props.text} - ${this.props.verseKey}`);
+    this.setState({ isCopied: true });
+
+    setTimeout(() => this.setState({ isCopied: false }), 1000);
+  };
+
+  render() {
+    const { isCopied } = this.state;
+
+    return (
+      <a
+        tabIndex="-1"
+        onClick={this.handleCopy}
+        className={!isCopied && 'text-muted'}
+        data-metrics-event-name="Ayah:Copy"
+      >
+        <i className="ss-icon ss-attach vertical-align-middle" />{' '}
+        <LocaleFormattedMessage
+          id={isCopied ? 'actions.copied' : 'actions.copy'}
+          defaultMessage={isCopied ? 'Copied!' : 'Copy'}
+        />
+      </a>
+    );
+  }
+}
+
+Copy.propTypes = {
+  text: PropTypes.string.isRequired,
+  verseKey: PropTypes.string.isRequired,
+};
+
+export default Copy;
---
+++ src/components/Copy/spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Copy from './index';
+
+const text = 'Some text';
+let wrapper;
+
+describe('<Copy />', () => {
+  beforeEach(() => {
+    wrapper = shallow(<Copy text={text} />);
+  });
+
+  it('should render', () => {
+    expect(wrapper).to.be.ok; // eslint-disable-line
+  });
+});

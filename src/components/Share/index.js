f7db063a6f288c1c4f7e1828fcee252e354b1cc1
zkariafisal <149721727+zkariafisal@users.noreply.github.com>
2026-05-27 01:15:36 +0300

    Add files via upload

---
+++ src/components/Share/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { ShareButtons, generateShareIcon } from 'react-share';
+import * as customPropTypes from 'customPropTypes';
+
+const styles = require('./style.scss');
+
+const { FacebookShareButton, TwitterShareButton } = ShareButtons;
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+
+const Share = ({ chapter, verse }) => {
+  // Fallback to Surah Id
+  let path;
+
+  if (verse) {
+    const translations = (verse.translations || [])
+      .map(translation => translation.resourceId)
+      .join(',');
+    path = `${verse.chapterId}/${verse.verseNumber}?translations=${translations}`;
+  } else {
+    path = chapter.chapterNumber;
+  }
+
+  const shareUrl = `https://quran.com/${path}`;
+  const title = verse
+    ? `Surah ${chapter.nameSimple} [${verse.verseKey}]`
+    : `Surah ${chapter.nameSimple}`;
+  const iconProps = verse ? { iconBgStyle: { fill: '#d1d0d0' } } : {};
+
+  return (
+    <div className={`${styles.shareContainer}`}>
+      <FacebookShareButton
+        url={shareUrl}
+        title={title}
+        windowWidth={670}
+        windowHeight={540}
+        className={`${styles.iconContainer}`}
+      >
+        <FacebookIcon size={24} round {...iconProps} />
+      </FacebookShareButton>
+      <TwitterShareButton
+        url={shareUrl}
+        title={title}
+        className={`${styles.iconContainer}`}
+      >
+        <TwitterIcon size={24} round {...iconProps} />
+      </TwitterShareButton>
+    </div>
+  );
+};
+
+Share.propTypes = {
+  chapter: customPropTypes.surahType.isRequired,
+  verse: customPropTypes.verseType
+};
+
+export default Share;

69246dda67899fc5677218740a524171f2df7bce
zkariafisal <149721727+zkariafisal@users.noreply.github.com>
2026-05-28 02:39:18 +0300

    Update index.js

--- src/components/IndexHeader/index.js
+++ src/components/IndexHeader/index.js
@@ -4,7 +4,8 @@ import SearchInput from 'components/SearchInput';
 import debug from 'helpers/debug';
 import Jumbotron from 'quran-components/lib/Jumbotron';
 
-const logo = require('../../../static/images/logo-lg-w.png');
+// تم تغيير هذا السطر: لم نعد نستخدم المسار المحلي، بل الرابط المباشر من GitHub
+const logo = "https://raw.githubusercontent.com/zkariafisal/Oreo-quiz-images/refs/heads/main/q.png";
 const styles = require('./style.scss');
 
 class IndexHeader extends Component {
@@ -28,7 +29,12 @@ class IndexHeader extends Component {
           <div className="row">
             <div className="col-md-10 col-md-offset-1 text-center">
               <Link to="/" className={styles.link} data-metrics-event-name="IndexHeader:Link:Index">
-                <img src={logo} className={styles.logo} alt="logo" />
+                {/*
+                  تم تغيير مصدر الصورة (src) لاستخدام المتغير الجديد `logo`
+                  الذي يحتوي على رابط الصورة من GitHub.
+                  سيبقى اسم الكلاس (className) كما هو للحفاظ على التنسيق.
+                */}
+                <img src={logo} className={styles.logo} alt="New Logo" />
               </Link>
               <h4 className={styles.title}>THE NOBLE QUR&#x27;AN</h4>
               {this.renderSearch()}
